12cfe9864b489d9d957f9b9a81502a56b34ee176
Dennis Dunn <ansofive@gmail.com>
2016-12-06 20:42:43 -0500

    The first tests for "get patient series" are
    passing.

--- Services/PatientSeriesService.cs
+++ Services/PatientSeriesService.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Refdata.SupportingData;
 using Vaxplan.Model;
 
 namespace Vaxplan.Services
 {
     public class PatientSeriesService
     {
-      public  IEnumerable<PatientSeriesDose> CreatePatientSeries(IEnumerable<AntigenSeriesDose> series)
+        public bool IsValidSeries(DateTime assessmentDate, PatientProfile profile, antigenSupportingDataSeries series)
         {
-            throw new NotImplementedException();
+            switch (series.seriesType)
+            {
+                case "Standard":
+                    var seriesGender = series.requiredGender[0];
+                    seriesGender = string.IsNullOrWhiteSpace(seriesGender) ? "F" : seriesGender;
+                    return seriesGender == profile.Gender;
+                case "Risk":
+                    // TODO Add date checks to the select call.
+                    return series.indication
+                        .Select(x => x.observationCode.code == profile.MedHistoryCode)
+                        .Aggregate(false, (x, y) => x || y);
+                default:
+                    throw new ArgumentException("Invalid series type.", "series");
+            }
         }
     }
-    
+
 }
--- UnitTests/PatientSeriesServiceShould.cs
+++ UnitTests/PatientSeriesServiceShould.cs
@@ -1,25 +1,40 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vaxplan.Services;
 
 namespace Vaxplan.UnitTests
 {
     [TestClass]
     public class PatientSeriesServiceShould
     {
-        private const string TESTCASE_ID = "2013-0102";
-        private const string ANTIGEN_ID = "Diphtheria";
+        private const string TESTCASE_ID = "2013-0531";
+        private const string ANTIGEN_ID = "Measles";
 
         [TestMethod]
-        public void PickValidAntigenSeries()
+        public void ValidateStandardAntigenSeries()
         {
-            var testcase = CaseLibrary.Testcases[TESTCASE_ID];
-            var antigen = Refdata.Antigens[ANTIGEN_ID];
-
+            var data = IsValid(0);
+            Assert.IsTrue(data);
         }
 
+
         [TestMethod]
-        public void SkipInvalidAntigenSeries()
+        public void ValidateAntigenRiskSeries()
         {
+            var data = IsValid(1);
+            Assert.IsTrue(data);
+        }
+
+        public bool IsValid(int seriesIndex)
+        {
+            var @case = CaseLibrary.Testcases[TESTCASE_ID];
+            var profile = @case.PatientProfile.ToModel();
+            profile.MedHistoryCode = "048";
+            var series = Refdata.Antigens[ANTIGEN_ID].series[seriesIndex];
+            var assessmentDate = DateTime.Parse(@case.ExpectedForecast.AssessmentDate);
+            var service = new PatientSeriesService();
+            return service.IsValidSeries(assessmentDate, profile, series);
         }
     }
 }
